0c07785e1248c36ebf6c0168ba1c4b939cb96073
Hayk Ghazaryan <ghazareon@gmail.com>
2025-03-10 18:19:25 +0400

    feat(cards - 600, 605, 610) markup cards

--- src/shared/config/designTokens/backgroundImage.ts
+++ src/shared/config/designTokens/backgroundImage.ts
@@ -6,6 +6,7 @@
 /* prettier-ignore */
 export const backgroundImage = {
 	"grad-aqua-l-100": "linear-gradient(180deg, #def9fa 0.89%, #bef3f5 17.23%, #9dedf0 42.04%, #7de7eb 55.12%, #5ce1e6 71.54%, #33bbcf 100%)",
+	"grad-aqua-l-200": "linear-gradient(90deg, #00f5a0 0%, #00d9f5 100%)",
 	"grad-aqua-r-100": "radial-gradient(64.33% 64.33% at 71.16% 35.69%, #def9fa 0.89%, #bef3f5 17.23%, #9dedf0 42.04%, #7de7eb 55.12%, #5ce1e6 71.54%, #33bbcf 100%)",
 
 	"grad-gray-l-100": "linear-gradient(135deg, #272727 0%, #11101d 100%)",
@@ -15,18 +16,5 @@ export const backgroundImage = {
 	"grad-gray-l-500": "linear-gradient(154deg, #fff 16.15%, #6d6d6d 57.81%, #11101d 93.23%)",
 	"grad-gray-l-600": "linear-gradient(154deg, #fff 0%, #14101d 100%)",
 
-/*
-	"grad-aqua-l-100": "linear-gradient(180deg, #def9fa 0.89%, #bef3f5 17.23%, #9dedf0 42.04%, #7de7eb 55.12%, #5ce1e6 71.54%, #33bbcf 100%)",
-	"grad-aqua-r-100": "radial-gradient(64.33% 64.33% at 71.16% 35.69%, rgb(222, 249, 250) 0.8877529762685299%, rgb(190, 243, 245) 17.229999601840973%, rgb(157, 237, 240) 42.03999936580658%, rgb(125, 231, 235) 55.11999726295471%, rgb(92, 225, 230) 71.5399980545044%, rgb(51, 187, 207) 100%)",
-
-	"grad-gray-l-100": "linear-gradient(135deg, rgba(39, 39, 39, 0.5) 0%, rgba(17, 16, 29, 0.5) 100%)",
-	"grad-gray-l-200": "linear-gradient(154deg, rgba(255, 255, 255, 0.2) 0%, #14101d 100%)",
-	"grad-gray-l-300": "linear-gradient(120deg, #fff 0%, #14101d 100%)",
-	"grad-gray-l-400": "linear-gradient(122deg, #fff 0%, #14101d 98.96%)",
-	"grad-gray-l-500": "linear-gradient(131deg, #fff 16.15%, #6d6d6d 57.81%, #11101d 93.23%)",
-	"grad-gray-l-600": "linear-gradient(131deg, #fff 16.15%, #6d6d6d 57.81%, #11101d 93.23%)",
-	"grad-gray-l-700": "linear-gradient(154deg, #fff 16.15%, #6d6d6d 57.81%, #11101d 93.23%)",
-	"grad-gray-l-800": "linear-gradient(149deg, #fff 0%, #14101d 90.6%)",
-	*/
 	"none": "none"
 };
